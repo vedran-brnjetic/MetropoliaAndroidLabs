e077dfbfef0374823313b512edca20bac1fb4005
vedran-brnjetic <Vedran.Brnjetic@metropolia.fi>
2018-03-22 13:15:47 +0200

    Lab 1B done

--- app/src/main/java/me/byelingual/projectzero/MainActivity.kt
+++ app/src/main/java/me/byelingual/projectzero/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        //sample_text.text = stringFromJNI()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
